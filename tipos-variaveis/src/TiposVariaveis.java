3f8850f7518229298706bd4367878ac89c6a4db2
Amanda Camargo <amanda_kamargo@hotmail.com>
2024-05-22 16:25:08 -0300

    Tipos e Variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,23 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //TIPOS PRIMITIVOS
+        //ESTUDAR CLASSE STRING QUE REPRESENTA TEXTO NA APLICACAO
+
+        String meuNome = "Amanda Camargo";
+        double salarioMinimo = 2500.33;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+        numero = 10;
+
+        System.out.print(numero);
+
+
+        final double VALOR_DE_PI = 3.14;
+
+
     }
 }

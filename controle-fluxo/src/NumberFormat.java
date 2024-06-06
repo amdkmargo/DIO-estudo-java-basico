050a5fe632b137393532ad3321e480e33a5e79a0
Amanda Camargo <amanda_kamargo@hotmail.com>
2024-06-06 15:27:54 -0300

    controle-de-fluxo-att

---
+++ controle-fluxo/src/NumberFormat.java
@@ -0,0 +1,9 @@
+
+public class NumberFormat {
+
+    public static Object getInstance() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getInstance'");
+    }
+
+}
---
+++ controle-fluxo/src/resultadoEscolar.java
@@ -0,0 +1,14 @@
+
+public class resultadoEscolar {
+
+    public static void main(String[] args) {
+        int nota = 3;
+        //  String resultado = nota >=7 ? "Aprovado" : "Reprovado";
+
+        //  System.out.println(resultado);
+        String resultado = nota >=7 ? "Aprovado" : nota >=5 && nota <7 ? "Recuperacao" : "Reprovado";
+
+        System.out.println(resultado);
+    }
+
+}
